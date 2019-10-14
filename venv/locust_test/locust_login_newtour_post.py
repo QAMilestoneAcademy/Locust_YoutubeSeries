99410213ff8f8202c8a659b9854f385339b58915
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2019-10-14 10:28:32 +0400

    locust youtube series

---
+++ venv/locust_test/locust_login_newtour_post.py
@@ -0,0 +1,21 @@
+#http://newtours.demoaut.com/login.php
+'''action: process
+userName: qamile1@gmail.com
+password: qamile
+login.x: 16
+login.y: 9'''
+
+from locust import HttpLocust, TaskSet,task
+
+class UserBehaviour(TaskSet):
+
+    @task
+    def login_post(self):
+        self.client.post("/login.php",{"action": "process","userName": "qamile1@gmail.com","password": "qamile"
+                                       ,"login.x": "16","login.y": "9"})
+
+class User(HttpLocust):
+    task_set=UserBehaviour
+    min_wait=5000
+    max_wait = 10000
+    host="http://newtours.demoaut.com"
