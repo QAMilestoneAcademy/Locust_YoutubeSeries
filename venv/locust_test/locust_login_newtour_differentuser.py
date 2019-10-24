247ff1c5c53da40bcfb6da12b3e7c673d892a982
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2019-10-24 10:52:06 +0400

    locust youtube series

---
+++ venv/locust_test/locust_login_newtour_differentuser.py
@@ -0,0 +1,32 @@
+from locust import HttpLocust, TaskSet,task
+
+UserName=[
+    ("qamile1@gmail.com","qamile"),
+    ("qamile2@gmail.com", "qamile"),
+    ("qamile3@gmail.com", "qamile"),
+    ("qamile4@gmail.com", "qamile"),
+    ("qamile5@gmail.com", "qamile")
+ ]
+
+
+class UserBehaviour(TaskSet):
+
+    def on_start(self):
+        self.userName="Not_exist"
+        self.password="Not_exist"
+        if len(UserName) >0:
+            self.userName, self.password = UserName.pop()
+
+            
+    @task
+    def login_post(self):
+        print(self.userName)
+        self.client.post("/login.php",data={"action": "process","userName": self.userName,"password": self.password
+                                       ,"login.x": "16","login.y": "9"})
+
+
+class User(HttpLocust):
+    task_set=UserBehaviour
+    min_wait=5000
+    max_wait = 10000
+    host="http://newtours.demoaut.com"
